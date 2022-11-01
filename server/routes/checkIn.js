36efa9396cfd24fdad490b79866949c8dc6f659d
keniabalestra <kenia.balestra@gmail.com>
2022-11-01 13:51:12 -0400

    Fix insert data to database

--- server/routes/checkIn.js
+++ server/routes/checkIn.js
@@ -1,39 +1,37 @@
 const express = require('express');
 const router = express.Router();
 // const checkIn = require('../db/queries/checkIn');
+const db = require('../configs/db.config');
 
-router.post("/checkin", (request, response) => {
+router.post("/", (request, response) => {
   if (process.env.TEST_ERROR) {
     setTimeout(() => response.status(500).json({}), 1000);
     return;
   }
 
   const {
+    location,
     name,
     time,
-    location,
-    reason,
     alergies,
+    medications,
     conditions,
-    medications } = request.body;
+    reason } = request.body;
 
-console.log("BODY:", request.body)
+  console.log("BODY:", request);
 
   db.query(
-    "INSERT INTO checkins (id, location_id, patient_name, time, allergies, medications, conditions, visit_reason) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-    [name,
+    "INSERT INTO checkins (location_id, patient_name, time, allergies, medications, conditions, visit_reason) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [location,
+      name,
       time,
-      reason, 
-      location,
       alergies,
+      medications,
       conditions,
-      medications]
-      )
+      reason]
+  )
     .then(() => {
-      setTimeout(() => {
-        response.status(204).json({});
-
-      }, 1000);
+      response.status(204).json({});
     })
     .catch(error => console.log(error));
 });
