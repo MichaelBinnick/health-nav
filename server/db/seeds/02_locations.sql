4be75e54ad4aa1fc0c758c0c1ad15e34d7044ed4
keniabalestra <kenia.balestra@gmail.com>
2022-11-02 22:02:07 -0400

    Update locations seeds file

--- server/db/seeds/02_locations.sql
+++ server/db/seeds/02_locations.sql
@@ -2,20 +2,21 @@
 -- locations seeds
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (1, 'front doors', 2, '9am', '5pm');
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (2, 'admin', 2, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (3, 'pediatric - waiting', 2, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (4, 'pediatric - prep & recovery', 1, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (5, 'adult - waiting', 2, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (6, 'adult - prep & recovery', 1, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (7, 'staff', 5, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (8, 'lab', 1, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (9, 'imaging', 1, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (10, 'emergency', 3, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (11, 'utility', 5, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (12, 'dietary', 5, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (13, 'surgery', 1, '9am', '5pm');
-INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (14, 'inpatient', 1, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (3, 'Pediatric - Waiting', 2, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (4, 'Pediatric - Prep & Recovery', 1, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (5, 'Adult/Inpatient - Waiting', 2, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (6, 'Adult - Prep & Recovery', 1, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (7, 'Staff', 5, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (8, 'Lab', 1, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (9, 'Imaging', 1, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (10, 'Emergency', 3, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (11, 'Utility', 5, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (12, 'Dietary', 5, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (13, 'Surgery', 1, '9am', '5pm');
+INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (14, 'Inpatient', 1, '9am', '5pm');
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (15, 'restroom', 4, '9am', '5pm');
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (16, 'restroom', 4, '9am', '5pm');
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (17, 'restroom', 4, '9am', '5pm');
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (18, 'restroom', 4, '9am', '5pm');
 INSERT INTO locations (id, name, department_id, hours_open, hours_close) VALUES (19, 'restroom', 4, '9am', '5pm');
+
