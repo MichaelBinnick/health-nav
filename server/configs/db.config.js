2575e6a00d1139e1deec0ebe658e41636a52fc2a
MichaelBinnick <mbinnick@live.ca>
2022-11-17 13:58:35 -0500

    adds bit.io db credentials to server db.config.js

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -1,14 +1,14 @@
 // Database connections
 const { Pool } = require('pg');
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
+// const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
 
 const pool = new Pool({
-	user: DB_USER,
-	host: DB_HOST,
-	password: DB_PASSWORD,
-	port: DB_PORT,
-	database: DB_DATABASE,
+	user: MichaelBinnick,
+	host: db.bit.io,
+	password: v2_3vdjq_j7w4Cb3QNmH9GWrhpbMbp5f,
+	port: 5432,
+	database: MichaelBinnick/health-nav,
 })
 
 pool.connect().then(() => {
